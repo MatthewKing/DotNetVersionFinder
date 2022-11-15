4e161d9a2c28c85be74fd7891a46f4342a49deb9
Matthew King <mking@mking.net>
2022-11-15 21:38:32 +0800

    Overhaul code. Support .NET CLI.

--- src/DotNetVersionFinder/DotNetVersion.cs
+++ src/DotNetVersionFinder/DotNetVersion.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.Win32;
+﻿using System.Diagnostics;
+using Microsoft.Win32;
 
 namespace DotNetVersionFinder;
 
@@ -8,61 +9,99 @@ namespace DotNetVersionFinder;
 public static class DotNetVersion
 {
     /// <summary>
-    /// Maps the minimum release keys to the associated versions.
+    /// Returns the highest version of .NET that is installed on this machine.
     /// </summary>
-    /// <remarks>
-    /// See https://docs.microsoft.com/en-us/dotnet/framework/migration-guide/minimum-release-dword
-    /// </remarks>
-    private static readonly SortedDictionary<int, Version> Versions = new SortedDictionary<int, Version>
+    /// <returns>The .NET version (or null if it was unable to be determined).</returns>
+    public static Version GetVersion()
     {
-        [378389] = new Version(4, 5),
-        [378675] = new Version(4, 5, 1),
-        [379893] = new Version(4, 5, 2),
-        [393295] = new Version(4, 6),
-        [394254] = new Version(4, 6, 1),
-        [394802] = new Version(4, 6, 2),
-        [460798] = new Version(4, 7),
-        [461308] = new Version(4, 7, 1),
-        [461808] = new Version(4, 7, 2),
-        [528040] = new Version(4, 8),
-        [533320] = new Version(4, 8, 1),
-    };
+        // First, we look for versions with the .NET CLI:
 
-    /// <summary>
-    /// Returns the .NET Framework version that is installed on this machine.
-    /// </summary>
-    /// <returns>The .NET Framework version that is installed on this machine.</returns>
-    public static Version Find()
-    {
-        var releaseKey = FindReleaseKey();
-        foreach (var version in Versions.OrderByDescending(x => x.Key))
+        var cliVersion = GetVersionFromDotNetCli();
+        if (cliVersion is not null)
         {
-            if (releaseKey >= version.Key)
-            {
-                return version.Value;
-            }
+            return cliVersion;
+        }
+
+        // Next, look for a .NET framework version:
+
+        var registryVersion = GetFrameworkVersionFromRegistry();
+        if (registryVersion is not null)
+        {
+            return registryVersion;
         }
 
         return null;
     }
 
     /// <summary>
-    /// Returns the release key of the .NET Framework version that is installed on this machine.
+    /// Returns the version of .NET that is installed on this machine, as reported by the dotnet CLI.
     /// </summary>
-    /// <returns>The release key of the .NET Framework version that is installed on this machine.</returns>
-    public static int? FindReleaseKey()
+    /// <returns>The .NET version (or null if it was unable to be determined).</returns>
+    public static Version GetVersionFromDotNetCli()
     {
-        return GetDotNetReleaseKeyFromRegistry();
+        try
+        {
+            var psi = new ProcessStartInfo();
+            psi.FileName = "dotnet";
+            psi.Arguments = "--version";
+            psi.UseShellExecute = false;
+            psi.CreateNoWindow = true;
+            psi.RedirectStandardOutput = true;
+
+            using var process = Process.Start(psi);
+
+            if (process is not null)
+            {
+                process.WaitForExit();
+
+                if (Version.TryParse(process.StandardOutput.ReadToEnd(), out var version))
+                {
+                    return version;
+                }
+            }
+        }
+        catch (Exception)
+        {
+            // Swallow all exceptions.
+        }
+
+        return null;
     }
 
     /// <summary>
-    /// Gets the .NET release key from the registry.
+    /// Returns the highest known version of the .NET Framework that is installed on this machine,
+    /// determined by reading the framework release key from the Windows registry.
     /// </summary>
-    /// <returns>The .NET release key, or null if unable to find the release key.</returns>
+    /// <returns>The .NET Framework version (or null if it was unable to be determined).</returns>
+    public static Version GetFrameworkVersionFromRegistry()
+    {
+        // See https://docs.microsoft.com/en-us/dotnet/framework/migration-guide/minimum-release-dword
+
+        return GetFrameworkReleaseKeyFromRegistry() switch
+        {
+            >= 533320 => new Version(4, 8, 1),
+            >= 528040 => new Version(4, 8),
+            >= 461808 => new Version(4, 7, 2),
+            >= 461308 => new Version(4, 7, 1),
+            >= 460798 => new Version(4, 7),
+            >= 394802 => new Version(4, 6, 2),
+            >= 394254 => new Version(4, 6, 1),
+            >= 393295 => new Version(4, 6),
+            >= 379893 => new Version(4, 5, 2),
+            >= 378675 => new Version(4, 5, 1),
+            >= 378389 => new Version(4, 5),
+            _ => null,
+        };
+    }
+
+    /// <summary>
+    /// Returns the .NET Framework release key (from the Windows registry).
+    /// </summary>
+    /// <returns>The .NET Framework release key, or null if unable to find the release key.</returns>
     /// <remarks>
     /// See https://docs.microsoft.com/en-us/dotnet/framework/migration-guide/how-to-determine-which-versions-are-installed
     /// </remarks>
-    private static int? GetDotNetReleaseKeyFromRegistry()
+    public static int? GetFrameworkReleaseKeyFromRegistry()
     {
 #if NET6_0_OR_GREATER
         if (!OperatingSystem.IsWindows())
