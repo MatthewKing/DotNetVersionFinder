5213f66de2faaf9921d508291cc87a3621eaf312
Matthew King <mking@mking.net>
2022-11-16 07:09:34 +0800

    Enable source link.

--- build/build.ps1
+++ build/build.ps1
@@ -1,4 +1,4 @@
 $root = Resolve-Path (Join-Path $PSScriptRoot "..")
 $project =  "$root/src/DotNetVersionFinder"
 $output = "$root/artifacts"
-dotnet msbuild "/t:Restore;Build;Pack" "/p:Configuration=Release" "/p:PackageOutputPath=$output" $project
+dotnet pack $project --output $output --configuration "Release" -p:ContinuousIntegrationBuild=true
